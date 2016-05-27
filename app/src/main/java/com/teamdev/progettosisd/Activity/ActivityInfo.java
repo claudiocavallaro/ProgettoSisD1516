68a6d9aadd07fe0ab36af5b8e13dc78e024d78a3
lords91 <claudio.cavallaro91@gmail.com>
2016-05-27 13:44:59 +0200

    Interfaccia

---
+++ app/src/main/java/com/teamdev/progettosisd/Activity/ActivityInfo.java
@@ -0,0 +1,15 @@
+package com.teamdev.progettosisd.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by claud on 27/05/2016.
+ */
+public class ActivityInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/teamdev/progettosisd/Activity/MainActivity.java
+++ app/src/main/java/com/teamdev/progettosisd/Activity/MainActivity.java
@@ -1,13 +1,18 @@
-package com.teamdev.progettosisd;
+package com.teamdev.progettosisd.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.teamdev.progettosisd.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +23,23 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button createButton = (Button) findViewById(R.id.buttonCreate);
+        Button joinButton = (Button) findViewById(R.id.buttonJoin);
+
+        createButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Ho cliccato create", Toast.LENGTH_LONG).show();
             }
         });
+
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Ho cliccato join", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
     @Override
@@ -37,16 +51,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_info) {
+            Intent i = new Intent(MainActivity.this, ActivityInfo.class);
+            startActivity(i);
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
